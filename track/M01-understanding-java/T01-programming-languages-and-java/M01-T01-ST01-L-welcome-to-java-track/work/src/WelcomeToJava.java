7fa7711b23319818fa36a58b3397dbbab6103493
shravana shinagari <shravanashinagari10@gmail.com>
2026-07-30 12:49:30 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
+       System.out.println("Welcome to Java Track");
+       System.out.println("I am excited to start my Java Journey");
+       System.out.println("I am confident that I will be a successful Java Developer");
     }
 }
