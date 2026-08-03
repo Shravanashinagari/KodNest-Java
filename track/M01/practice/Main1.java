9d78be7f58c3f07ce4f3bda7ad4b5c830fd38a1d
shravana shinagari <shravanashinagari10@gmail.com>
2026-08-03 16:04:52 +0530

    Practice program

---
+++ track/M01/practice/Main1.java
@@ -0,0 +1,14 @@
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        String learnerName = "Shravana Shinagari";
+        int completedProblems = 50;
+        double practiceHours = 5;
+        boolean projectCompleted = true;
+        System.out.println("Learner Name: " + learnerName);
+        System.out.println("Completed Problems: " + completedProblems);
+        System.out.println("Practice Hours: " + practiceHours);
+        System.out.println("Project Completed: " + projectCompleted);
+    }
+}
