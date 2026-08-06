4c59f9355c044d53f8d4ee39006d89043455d557
shravana shinagari <shravanashinagari10@gmail.com>
2026-08-06 13:01:30 +0530

    added

---
+++ track/M02/T02/LabelLoops.java
@@ -0,0 +1,18 @@
+
+public class LabelLoops {
+
+    public static void main(String[] args) {
+        outer:
+        for (int i = 1; i <= 5; i++) {
+            inner:
+            for (int j = 1; j <= 4; j++) {
+                if (i == 3) {
+                    break outer;
+                }
+                System.out.println("i: " + i + " " + "j : " + j);
+            }
+            System.out.println();
+        }
+
+    }
+}
