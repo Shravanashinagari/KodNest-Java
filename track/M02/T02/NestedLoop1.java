f21ff5d56266b711396fbe1673645f34190505ce
shravana shinagari <shravanashinagari10@gmail.com>
2026-08-06 12:00:55 +0530

    added new

--- track/M02/T02/NestedLoop1.java
+++ track/M02/T02/NestedLoop1.java
@@ -5,7 +5,7 @@ public class NestedLoop1 {
         for (int i = 1; i <= 5; i++) {
 
             for (int j = 1; j <= 5; j++) {
-                System.out.println(j);
+                System.out.print(j + " ");
             }
             System.out.println();
 
---
+++ track/M02/T02/NestedLoop3.java
@@ -0,0 +1,20 @@
+
+public class NestedLoop3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+
+            int j = 1;
+
+            do {
+
+                System.out.print(j + " ");
+                j++;
+            } while (j <= 5);
+            System.out.println();
+            i++;
+
+        } while (i <= 5);
+    }
+}
