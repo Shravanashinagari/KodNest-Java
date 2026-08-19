0b64f83c51c9f75e0217620f81dbb1d785eb7f98
shravana shinagari <shravanashinagari10@gmail.com>
2026-08-20 00:50:19 +0530

    commit

---
+++ track/M02/constructor/StudentApplication.java
@@ -0,0 +1,31 @@
+
+class Student {
+
+    String name;
+    int age;
+    double height;
+
+    Student(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+    }
+}
+
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Shravana", 22, 5.2);
+
+        Student s2 = new Student("Sonu", 21, 5.3);
+
+        s1.display();
+        s2.display();
+    }
+}
